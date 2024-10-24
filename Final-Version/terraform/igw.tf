65500e3abc2493befa2183f8506b7165cd596ac6
Ahmed Saber <153558858+AhSaber11@users.noreply.github.com>
2024-10-24 23:24:53 +0300

    Delete Final-Version/terraform directory

--- Final-Version/terraform/igw.tf
+++
@@ -1,4 +0,0 @@
-#create the Internet gatway
-resource "aws_internet_gateway" "depigw" {
-  vpc_id = aws_vpc.depivpc.id
-}
--- Final-Version/terraform/vpc.tf
+++
@@ -1,4 +0,0 @@
-#create a vpc
-resource "aws_vpc" "depivpc" {
-  cidr_block = "10.0.0.0/16"
-}
